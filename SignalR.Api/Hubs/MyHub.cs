46591374ca1bd42e243cd300ea02e43ba6a2fdf2
Yiğitcan Ölmez <yigitcan.olmez@d-teknoloji.com.tr>
2024-02-05 22:09:20 +0300

    added groups.

--- SignalR.Api/Hubs/MyHub.cs
+++ SignalR.Api/Hubs/MyHub.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using SignalR.Api.Models;
 
 namespace SignalR.Api.Hubs
 {
@@ -7,6 +10,14 @@ namespace SignalR.Api.Hubs
         private static List<string> names = new List<string>();
         public static int clientCount { get; set; } = 0;
         public static int TeamCount { get; set; } = 7;
+
+        private readonly AppDbContext _appDbContext;
+
+        public MyHub(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
         public async Task SendName(string name)
         {
 
@@ -29,6 +40,55 @@ namespace SignalR.Api.Hubs
         {
             await Clients.All.SendAsync("ReceiveNames", names);
         }
+
+        public async Task AddToGroup(string teamName) =>
+            await Groups.AddToGroupAsync(Context.ConnectionId, teamName);
+
+        public async Task SendNameByGroup(string name, string teamName)
+        {
+            var team = await _appDbContext.Teams.FirstOrDefaultAsync(x => x.Name == teamName);
+
+            if (team != null)
+            {
+                team.Users.Add(new User
+                {
+                    Name = name
+                });
+            }
+            else
+            {
+                var newTeam = new Team
+                {
+                    Name = teamName
+                };
+                newTeam.Users.Add(new User
+                {
+                    Name = name
+                });
+
+                await _appDbContext.Teams.AddAsync(newTeam);
+            }
+            await _appDbContext.SaveChangesAsync();
+
+            await Clients.Group(teamName).SendAsync("ReceiveMessageByGroup", name, teamName);
+
+        }
+
+        public async Task GetNamesByGroup()
+        {
+            var teams = _appDbContext.Teams.Include(x => x.Users).Select(y => new
+            {
+                teamName = y.Name,
+                users = y.Users.ToList(),
+            });
+
+            await Clients.All.SendAsync("ReceiveNamesByGroup", teams);
+        }
+
+        public async Task RemoveFromGroup(string teamName) =>
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, teamName);
+
+
 
         public async override Task OnConnectedAsync()
         {
--- SignalR.Api/Models/Team.cs
+++ SignalR.Api/Models/Team.cs
@@ -4,10 +4,10 @@
     {
         public Team()
         {
-            Users = new List<User>();    
+            Users = new List<User>();
         }
         public int Id { get; set; }
-        public int Name { get; set; }
+        public string Name { get; set; }
         public virtual ICollection<User> Users { get; set; }
     }
 }
